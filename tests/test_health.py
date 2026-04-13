9c618f13dca9af05b70383f97d07237a746ebebd
Sebitas <87529848+SebasCouto@users.noreply.github.com>
2026-04-13 11:21:17 -0300

    Update test_health.py

--- tests/test_health.py
+++ tests/test_health.py
@@ -6,8 +6,10 @@ import pytest
 client = TestClient(app)
 
 @pytest.mark.payments
-def test_healthcheck_returns_ok():
-    response = client.get("/health")
-
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
+class Payments:
+    
+    def test_healthcheck_returns_ok():
+        response = client.get("/health")
+    
+        assert response.status_code == 200
+        assert response.json() == {"status": "ok"}
